4975340d3e0128c2974989748b3ad968cc0dbeff
Maria <maria.guerra@zup.com.br>
2020-09-28 22:39:02 -0300

    create userService and controller methods

--- src/main/java/com/biblioteca/gerenciador/controller/UsuarioController.java
+++ src/main/java/com/biblioteca/gerenciador/controller/UsuarioController.java
@@ -1,9 +1,15 @@
 package com.biblioteca.gerenciador.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +24,45 @@ public class UsuarioController {
 	private UsuarioService service;
 
 	@GetMapping(value = "/usuario/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public UsuarioDTO getUsuario(@PathVariable("id") Integer id) {
+	public ResponseEntity<UsuarioDTO> getUsuario(@PathVariable("id") Integer id) {
 
-		return service.getById(id);
-
+		UsuarioDTO usuarioDto = service.getById(id);
+		
+		return new ResponseEntity<UsuarioDTO>(usuarioDto, HttpStatus.OK);
 	}
-
+	
+	@GetMapping(value = "/usuario/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UsuarioDTO> getByName(@PathVariable("name") String name){
+		
+		UsuarioDTO userFind = service.getByName(name);
+		
+		return new ResponseEntity<UsuarioDTO>(userFind, HttpStatus.OK);
+		
+	}
+	
+	
+	@PostMapping(value = "/usuario", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UsuarioDTO> saveUser(@RequestBody UsuarioDTO dto){
+		
+		UsuarioDTO userSaved = service.saveUser(dto);
+		
+		return new ResponseEntity<UsuarioDTO>(userSaved, HttpStatus.CREATED);
+		
+	}
+	@DeleteMapping(value = "/usuario/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable("id") Integer id){
+		
+		service.deleteUser(id);
+		
+		return ResponseEntity.ok().build();
+		
+	}
+	
+	@PutMapping(value = "/usuario", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UsuarioDTO> updatedUser(@RequestBody UsuarioDTO userUpdated){
+		
+		UsuarioDTO updatedUser = service.updatedUser(userUpdated);
+		
+		return new ResponseEntity<UsuarioDTO>(updatedUser, HttpStatus.OK);
+	}
 }
---
+++ src/main/java/com/biblioteca/gerenciador/service/UsuarioServiceImpl.java
@@ -0,0 +1,40 @@
+package com.biblioteca.gerenciador.service;
+
+import org.springframework.stereotype.Service;
+
+import com.biblioteca.gerenciador.dto.UsuarioDTO;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService  {
+
+	@Override
+	public UsuarioDTO saveUser(UsuarioDTO usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UsuarioDTO updatedUser(UsuarioDTO usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UsuarioDTO getById(Integer idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UsuarioDTO getByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	} 
+
+}
